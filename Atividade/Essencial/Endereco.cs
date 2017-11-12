b87bccec43e6a5398a26d32fb1010fafde6f5db2
Alander Machado <alandermm@hotmail.com>
2017-11-12 18:34:16 -0200

    Classe Endereco

---
+++ Atividade/Essencial/Endereco.cs
@@ -0,0 +1,11 @@
+namespace Atividade.Essencial
+{
+    public class Endereco
+    {
+        public string Logradouro {get; set;}
+        public string Bairro {get; set;}
+        public string Numero {get; set;}
+        public string Complemento {get; set;}
+        public string Cidade {get; set;}
+    }
+}
