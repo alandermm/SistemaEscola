27d17cfb019bff0139baa0a05bb3cd73ecffa823
Alander Machado <alandermm@hotmail.com>
2017-11-12 18:35:53 -0200

    Classe Sala

---
+++ Atividade/Essencial/Sala.cs
@@ -0,0 +1,10 @@
+namespace Atividade.Essencial
+{
+    public class Sala
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string TipoSala { get; set; }
+        public int Capacidade { get; set; }
+    }
+}
